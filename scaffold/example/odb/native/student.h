ac24450e3b86ecf6e02e0612c2ff6eb67b87f770
shilinnull <shilinnull@163.com>
2026-02-22 10:03:36 +0000

    obd操作、分组查询、视图查询、封装mysql

--- scaffold/example/odb/native/student.h
+++ scaffold/example/odb/native/student.h
@@ -2,7 +2,6 @@
 #define VIDEOPLAYER_STUDENT_H
 
 #include <string>
-#include <cstddef> // std::size_t
 #include <boost/date_time/posix_time/posix_time.hpp>
 #include <odb/nullable.hxx>
 #include <odb/core.hxx>
@@ -14,6 +13,8 @@ public:
     Student(const std::string &name): _name(name) {}
     size_t sn() const { return _sn; }
     void set_sn(size_t sn) { _sn = sn; }
+    size_t classes_id() const { return _classes_id; }
+    void set_classes_id(size_t classes_id) { _classes_id = classes_id; }
     const std::string &name() const { return _name; }
     void set_name(const std::string &name) { _name = name; }
     odb::nullable<int> age() const { return _age; }
@@ -26,12 +27,20 @@ private:
     friend class odb::access;
 #pragma db id auto
     size_t _sn;
+#pragma db index
+    size_t _classes_id;
     std::string _name;
     odb::nullable<int> _age;
     odb::nullable<double> _score;
     odb::nullable<boost::posix_time::ptime> _birthday;
 };
 
-
+//  select * from tbl where xxx limit n offset n
+#pragma db view query("select name, classes_id, score from tbl_student")
+struct NativeStudent {
+    std::string name;
+    size_t classes_id;
+    odb::nullable<double> score;
+};
 
 #endif //VIDEOPLAYER_STUDENT_H
