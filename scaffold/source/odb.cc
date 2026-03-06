5bb4a51d1ba5250b8a5b5ebf2e4189b8b9f316f8
shilinnull <shilinnull@163.com>
2026-03-06 07:46:56 +0000

    代码格式化

--- scaffold/source/odb.cc
+++ scaffold/source/odb.cc
@@ -1,14 +1,15 @@
 #include "odb.h"
 
 namespace Biliodb {
-    std::shared_ptr<odb::database> DBFactory::mysql(const mysql_settings &settings) {
+    std::shared_ptr<odb::database> DBFactory::mysql(const mysql_settings& settings) {
         // 1. 创建连接池
         std::unique_ptr<odb::mysql::connection_factory> pool(
             new odb::mysql::connection_pool_factory(settings.connection_pool_size));
         // 2. 创建数据库操作句柄
         auto handler = std::make_shared<odb::mysql::database>(settings.user,
-            settings.passwd, settings.db, settings.host, settings.port, nullptr,
-            settings.cset, 0, std::move(pool));
+                                                              settings.passwd, settings.db, settings.host,
+                                                              settings.port, nullptr,
+                                                              settings.cset, 0, std::move(pool));
         return handler;
     }
 }
--- scaffold/source/util.cc
+++ scaffold/source/util.cc
@@ -2,31 +2,84 @@
 #include "log.h"
 
 namespace Biliutil {
-std::optional<std::string> JSON::serialize(const Json::Value& val, bool styled) {
-    Json::StreamWriterBuilder builder;
-    if (!styled) {
-        builder["indentation"] = "";
+    std::optional<std::string> JSON::serialize(const Json::Value& val, bool styled) {
+        Json::StreamWriterBuilder builder;
+        if (!styled) { builder["indentation"] = ""; }
+        std::unique_ptr<Json::StreamWriter> swp(builder.newStreamWriter());
+        std::stringstream ss;
+        int ret = swp->write(val, &ss);
+        if (ret != 0) {
+            ERR("序列化失败！");
+            return std::optional<std::string>();
+        }
+        return ss.str();
     }
-    std::unique_ptr<Json::StreamWriter> swp(builder.newStreamWriter());
-    std::stringstream ss;
-    int ret = swp->write(val, &ss);
-    if (ret != 0) {
-        ERR("序列化失败！");
-        return std::optional<std::string>();
-    }
-    return ss.str();
-}
-std::optional<Json::Value> JSON::unserialize(const std::string& input) {
-    Json::CharReaderBuilder builder;
-    std::unique_ptr<Json::CharReader> reader(builder.newCharReader());
-    Json::Value val;
-    std::string errs;
-    bool ret = reader->parse(input.c_str(), input.c_str() + input.size(), &val, &errs);
-    if (ret == false) {
-        ERR("{} 反序列化失败: {}", input, errs);
-        return std::optional<Json::Value>();
-    }
-    return val;
-}
 
+    std::optional<Json::Value> JSON::unserialize(const std::string& input) {
+        Json::CharReaderBuilder builder;
+        std::unique_ptr<Json::CharReader> reader(builder.newCharReader());
+        Json::Value val;
+        std::string errs;
+        bool ret = reader->parse(input.c_str(), input.c_str() + input.size(), &val, &errs);
+        if (ret == false) {
+            ERR("{} 反序列化失败: {}", input, errs);
+            return std::optional<Json::Value>();
+        }
+        return val;
+    }
+
+    bool FUTIL::read(const std::string& filename, std::string& body) {
+        std::ifstream ifs;
+        ifs.open(filename, std::ios::in | std::ios::binary);
+        if (ifs.is_open() == false) {
+            ERR("打开文件失败: {}", filename);
+            return false;
+        }
+        ifs.seekg(0, std::ios::end);
+        size_t flen = ifs.tellg();
+        ifs.seekg(0, std::ios::beg);
+        body.resize(flen);
+        ifs.read(&body[0], flen);
+        if (ifs.good() == false) {
+            ERR("读取文件数据失败: {}", filename);
+            ifs.close();
+            return false;
+        }
+        ifs.close();
+        return true;
+    }
+
+    bool FUTIL::write(const std::string& filename, const std::string& body) {
+        std::ofstream ofs;
+        ofs.open(filename, std::ios::out | std::ios::binary | std::ios::trunc);
+        if (ofs.is_open() == false) {
+            ERR("打开文件失败: {}", filename);
+            return false;
+        }
+        ofs.write(body.c_str(), body.size());
+        if (ofs.good() == false) {
+            ERR("写入文件数据失败: {}", filename);
+            ofs.close();
+            return false;
+        }
+        ofs.close();
+        return true;
+    }
+
+    size_t STR::split(const std::string& src, const std::string& sep, std::vector<std::string>& dst) {
+        size_t pos, idx = 0;
+        while (1) {
+            pos = src.find(sep, idx);
+            if (pos == std::string::npos) { break; }
+            // 跳过空白的
+            if (pos == idx) {
+                idx = pos + sep.size();
+                continue;
+            }
+            dst.push_back(src.substr(idx, pos - idx));
+            idx = pos + sep.size();
+        }
+        if (idx < src.size()) { dst.push_back(src.substr(idx)); }
+        return dst.size();
+    }
 }
